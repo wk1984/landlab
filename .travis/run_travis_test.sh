511e453d7b8cee1a76e653bc507e342a30fd4841
mcflugen <mcflugen@gmail.com>
2015-01-25 20:56:45 -0700

    Print details about python environment.

--- .travis/run_travis_test.sh
+++ .travis/run_travis_test.sh
@@ -9,6 +9,8 @@ run_test()
 
   INSTALLDIR=$($PYTHON -c "import os; import landlab; print(os.path.dirname(landlab.__file__))")
 
+  echo $(which python)
+  echo $(ls /home/travis/miniconda/envs/test-env/lib/python2.7/site-packages)
   echo $(python -c 'import yaml; print(yaml.__path__)')
   conda list
   coverage run --source=$INSTALLDIR ../scripts/test-installed-landlab.py --doctest && (coverage report && cp .coverage ..)
