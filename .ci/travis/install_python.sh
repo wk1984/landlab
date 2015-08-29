238d82a65490088a27c6dca373c01e471c689e2a
mcflugen <mcflugen@gmail.com>
2015-08-29 10:05:44 -0600

    Install numpydoc with conda.

--- .ci/travis/install_python.sh
+++ .ci/travis/install_python.sh
@@ -17,7 +17,7 @@ hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update conda
 conda info -a
-cat requirements.txt | grep -v numpydoc | xargs conda create -n test-env python=$TRAVIS_PYTHON_VERSION
+cat requirements.txt | xargs conda create -n test-env python=$TRAVIS_PYTHON_VERSION
 source activate test-env
 conda install coverage
 conda install sphinx
