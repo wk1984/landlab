b8a362e874ce68023e53d3b2fa03315e9a700b89
mcflugen <mcflugen@gmail.com>
2016-05-14 23:37:16 -0600

    Added conda recipe; Added buid/upload script.

---
+++ .ci/travis/deploy_to_anaconda.sh
@@ -0,0 +1,19 @@
+if [[ "$TRAVIS_TAG" == v* ]]; then
+  echo "Installing deployment requirements."
+  conda install conda-build anaconda-client
+
+  file_to_upload=$(conda build --output --python=$TRAVIS_PYTHON_VERSION --numpy=$NUMPY_VERSION .conda)
+
+  echo "Building conda package"
+  conda build .conda -c landlab > stdout || exit -1
+
+  echo "Uploading to Anaconda"
+  anaconda -t $ANACONDA_TOKEN upload --force --user landlab \
+    --channel main $file_to_upload
+
+  echo "Done."
+else
+  echo "Not deploying."
+  echo "Tag is... $TRAVIS_TAG"
+  echo "Branch name is... $TRAVIS_BRANCH"
+fi
