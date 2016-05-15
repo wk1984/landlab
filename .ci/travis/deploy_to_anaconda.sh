bc7a0833f5518c3b1728b198717baff9e29ffd53
mcflugen <mcflugen@gmail.com>
2016-05-15 09:24:00 -0600

    Changed to upload to dev channel if not a release.

--- .ci/travis/deploy_to_anaconda.sh
+++ .ci/travis/deploy_to_anaconda.sh
@@ -1,6 +1,7 @@
+echo "Installing deployment requirements."
+ conda install conda-build anaconda-client
+
 if [[ "$TRAVIS_TAG" == v* ]]; then
-  echo "Installing deployment requirements."
-  conda install conda-build anaconda-client
 
   file_to_upload=$(conda build --output --python=$TRAVIS_PYTHON_VERSION --numpy=$NUMPY_VERSION .conda)
 
@@ -8,11 +9,21 @@ if [[ "$TRAVIS_TAG" == v* ]]; then
   conda build .conda -c landlab > stdout || exit -1
 
   echo "Uploading to Anaconda"
-  anaconda -t $ANACONDA_TOKEN upload --force --user landlab \
-    --channel main $file_to_upload
+  anaconda -t $ANACONDA_TOKEN upload --force --user landlab --channel main $file_to_upload
 
   echo "Done."
 else
+  export BUILD_STR='dev'
+
+  file_to_upload=$(conda build --output --python=$TRAVIS_PYTHON_VERSION --numpy=$NUMPY_VERSION .conda)
+
+  echo "Building conda package"
+  conda build .conda -c landlab > stdout || exit -1
+
+  echo "Uploading to Anaconda"
+  anaconda -t $ANACONDA_TOKEN upload --force --user landlab --channel $BUILD_STR $file_to_upload
+
+  echo "Done."
   echo "Not deploying."
   echo "Tag is... $TRAVIS_TAG"
   echo "Branch name is... $TRAVIS_BRANCH"
