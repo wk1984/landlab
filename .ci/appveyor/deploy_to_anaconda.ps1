8ca13fb635df25406a7db8d1efae260985c2fed7
mcflugen <mcflugen@gmail.com>
2016-05-15 19:33:22 -0600

    Build with conda on AppVeyor.

--- .ci/appveyor/deploy_to_anaconda.ps1
+++ .ci/appveyor/deploy_to_anaconda.ps1
@@ -9,11 +9,12 @@ if (($env:appveyor_repo_tag -eq "true") -and ($env:appveyor_repo_tag_name.Starts
 
 Invoke-Expression "conda config --set anaconda_upload no"
 
-write-output "Building package..."
-Invoke-Expression "conda build .conda -c landlab"
+# write-output "Building package..."
+# Invoke-Expression "conda build .conda -c landlab"
 
 $file_to_upload = (conda build --output .conda) | Out-String
-write-output "Uploading file $file_to_upload..."
+write-output "Uploading $file_to_upload..."
 Invoke-Expression "anaconda -t $env:ANACONDA_TOKEN upload --force --user landlab --channel $channel $file_to_upload"
+# Invoke-Expression "anaconda -t $env:ANACONDA_TOKEN upload --force --user landlab --channel $channel C:\\Miniconda\conda-bld\**\landlab*bz2"
 
 write-output "OK"
